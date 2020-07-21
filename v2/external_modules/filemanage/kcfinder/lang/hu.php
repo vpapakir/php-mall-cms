1687bfcd07a904aa53426617ac6a6df94117539a
Evangelos Papakirykou <vpapakir@gmail.com>
2020-07-21 10:04:37 +0100

    upgraded kcfinder from latest master on github

--- v2/external_modules/filemanage/kcfinder/lang/hu.php
+++ v2/external_modules/filemanage/kcfinder/lang/hu.php
@@ -6,6 +6,8 @@
 
 $lang = array(
 
+    '_lang' => "Hungarian",
+    '_native' => "Magyar",
     '_locale' => "hu_HU.UTF-8",  // UNIX localization code
     '_charset' => "utf-8",       // Browser charset
 
@@ -120,6 +122,11 @@ $lang = array(
     "Yes" => "Igen",
     "No" => "Nem",
     "You cannot rename the extension of files!" => "Nem változtathatja meg a fájlok kiterjezstését",
+    "Uploading file {number} of {count}... {progress}" => "A(z) {number}. fájl feltöltése (összesen {count}) ... {progress}",
+    "Failed to upload {filename}!" => "Nem sikerült feltölteni a '{filename}' fájlt.",
+    "Close" => "Bezár",
+    "Previous" => "Előző",
+    "Next" => "Következő",
+    "Confirmation" => "Megerősítés",
+    "Warning" => "Figyelem"
 );
-
-?>
